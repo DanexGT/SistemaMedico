d27c8fb31fd8079809c55892ac330441a90e22b0
Daniel <68794532+DanexGT@users.noreply.github.com>
2022-07-29 16:03:49 -0600

    Creación Esquemas en Query y Clase de Cadena de Conexion

---
+++ API/Datos/Conexion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class Conexion
+    {
+        private static string Usuario = "sa";
+        private static string Contrasenia = "1234";
+        private static string Servidor = "Danex\\SQLExpress";
+        private static string BaseDeDatos = "BDSistemaMedico";
+
+        public static string CadenaConexionSQL()
+        {
+            return "Persist Security Info = False; User ID = '" + Usuario
+                + "'; Contrasenia = '" + Contrasenia
+                + "'; Initial Catalog = '" + BaseDeDatos
+                + "'; Server =" + Servidor + "'";
+    }
+}
